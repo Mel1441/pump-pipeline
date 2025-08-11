4384869f845e51c6cfbb27e9f20dec433d1dd39a
Mel1441 <masonlm68@gmail.com>
2025-08-11 13:47:33 -0400

    Initial commit of my project

---
+++ dagster/alert_alarm.py
@@ -0,0 +1,89 @@
+from api import * 
+from api.tandemsource import TandemSourceApi
+from datetime import date
+from dotenv import load_dotenv
+import pandas as ps
+import os
+
+import dagster as dg
+
+
+
+@dg.asset 
+
+def get_alert_alarm_data() -> dg.MaterializeResult:
+    load_dotenv()
+    pump_id = os.getenv('PUMP_ID')
+    today = date.today()
+    data = TandemSourceApi().pump_events(pump_id, today, today,('4,5'))
+    df = ps.DataFrame(data)
+
+    return dg.MaterializeResult(metadata={"preview":dg.MetadataValue.md(df.to_markdown(index=False)),})
+
+hourly_alert_job = dg.define_asset_job(
+    "hourly_alert", selection=["get_alert_alarm_data"]
+)
+
+hourly_schedule = dg.ScheduleDefinition(
+    job=hourly_alert_job,
+    cron_schedule="0 * * * *"
+)
+
+@dg.asset 
+
+def fetch_all_events():
+    load_dotenv()
+    pump_id = os.getenv('PUMP_ID')
+    today = date.today()
+    print(today)
+    data = TandemSourceApi().pump_events(pump_id, today, today)
+    event_data = []
+    unparsed_events = []
+    for e in data:
+        if isinstance (e, dict):
+            event_data.append(e)
+        else:
+            unparsed_events.append(e)
+    path = os.getenv('PATH') 
+
+    event_path = f'{path}/{today}/daily/events/{pump_id}'
+    if not os.path.isdir(event_path):
+      os.makedirs(event_path)
+      print("Directory created successfully!")
+    else:
+      print("Directory already exists!")   
+
+    unparsed_path = f'{path}/{today}/daily/unparsed/{pump_id}' 
+    if not os.path.isdir(unparsed_path):
+      os.makedirs(unparsed_path)
+      print("Directory created successfully!")
+    else:
+      print("Directory already exists!")  
+    
+    with open(f'{event_path}/events-{pump_id}-{today}', 'w') as f:
+        f.write(event_data)  
+
+    with open(f'{unparsed_path}/unparsed-{pump_id}-{today}', 'w') as f:
+        f.write(unparsed_events)  
+      
+
+
+    return print('job done')
+
+
+
+daily_job = dg.define_asset_job(
+    "daily_job", selection=["fetch_all_events"]
+)
+
+daily_schedule = dg.ScheduleDefinition(
+    job=daily_job,
+    cron_schedule="0 0 * * *"
+)
+
+
+defs = dg.Definitions(
+    assets=[get_alert_alarm_data, fetch_all_events],
+    jobs=[hourly_alert_job, daily_job],
+    schedules=[hourly_schedule, daily_schedule],
+)
